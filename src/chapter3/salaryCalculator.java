47628b2a5c73dcb0f1a7f96daabe35a6290e320b
YOUNIS <abdoyounis02@gmail.com>
2024-02-04 01:42:43 +0200

    Learning If and If else statements

--- src/chapter3/salaryCalculator.java
+++ src/chapter3/salaryCalculator.java
@@ -1,5 +1,7 @@
 package chapter3;
 
+import java.util.Scanner;
+
 /*
 IF statement.
 All salespeople get a payment of 1000$ a week.
@@ -7,18 +9,25 @@ Salespeople who exceed 10 sales get an additional bonus of 250$
  */
 public class salaryCalculator {
 
-    public static void main(String arg[]){
+    public static void main(String[] arg){
 
         //Initialize known values
         int salary = 1000 ;
         int bonus = 250 ;
+        int quota = 10 ;
 
         //Get valus for the unknown
         System.out.println("How many sales did the employee make this week?");
-        Scanner scanner = new
-        //Quick detour for the bonus earnes
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
 
+        //Quick detour for the bonus earnes
+        if (sales > quota) {
+            salary = salary + bonus;
+        }
         //Output
+        System.out.println("The employee's salary is $" + salary);
     }
 
 }
